85bfcc3a322878c2c655d23bb91505c408520747
malekabdelkader <malekabdelkader2@gmail.com>
2021-02-28 13:51:47 +0100

    CRUD Product_Familly updated

--- Controllers/AnnounceControllers/EnchereController.js
+++ Controllers/AnnounceControllers/EnchereController.js
@@ -1,54 +1,71 @@
-const Enchere= require('../../model/Announce/Enchere.model')
-exports.addEnchere=async (req,res)=>{
-    try{
-        const {subject,details,city,phone,image,end_Date,initial_price}=req.body
-        user = req.params.UserID;
-        subcategorie = req.params.SubcategID;
-        const enchere =new Enchere({
-            subject,
-            details,
-            //city,
-            //user,
-            phone,
-            image,
-           // subcategorie,
-            end_Date,
-            initial_price
-        })
-        console.log(enchere)
-        const savedEnchere=await enchere.save();
-        res.status(200).json(savedEnchere)
-    }catch(err){
-        res.status(200).json({Error:err.message})
-    }
-}
-exports.getAll=async (req,res)=>{
-    try{
-        const enchers=await Enchere.find();
-        res.status(200).json(enchers)
+const Enchere = require("../../model/Announce/Enchere.model");
+const City = require("../../model/City.model");
+const Subcateg = require("../../model/Subcategs.model");
+const User = require("../../model/personne/User.model");
 
-    }catch(err){
-        res.status(400).json({Error :err.message})
-    }
-}
+exports.addEnchere = async (req, res) => {
+  try {
+    const {
+      subject,
+      details,
+      phone,
+      image,
+      end_Date,
+      initial_price,
+    } = req.body;
+    const { city, user, subcategorie } = req.params;
+    await City.findById(city);
+    await User.findById(user);
+    await Subcateg.findById(subcategorie);
+    const enchere = new Enchere({
+      subject,
+      details,
+      city,
+      user,
+      phone,
+      image,
+      subcategorie,
+      end_Date,
+      initial_price,
+    });
+    const saved = await enchere.save();
+    await Subcateg.findByIdAndUpdate(subcategorie, {
+      $push: { announces: saved._id },
+    });
+    await User.findByIdAndUpdate(user, {
+      $push: { announces: saved._id },
+    });
+    res.status(200).json(saved);
+  } catch (err) {
+    res.status(200).json({ Error: err.message });
+  }
+};
+exports.getAll = async (req, res) => {
+  try {
+    const enchers = await Enchere.find();
+    res.status(200).json(enchers);
+  } catch (err) {
+    res.status(400).json({ Error: err.message });
+  }
+};
 exports.UpDatedEnchere = async (req, res) => {
-    const { id } = req.params;
-    const updatedAnnonce = req.body;
-  
-    try {
-      const Rst = await Enchere.findByIdAndUpdate(
-        id,
-        { $set: updatedAnnonce },
-        { new: true }
-      );
-      if (Rst) {
-        await res
-          .status(200)
-          .json({ message: "Enchere updated!", updatedAnnonce });
-      } else {
-        throw new Error("EnchereID undefined !");
-      }
-    } catch (err) {
-      res.status(400).json({ Error: err.message });
+  const { id } = req.params;
+  const updatedAnnonce = req.body;
+
+  try {
+    const Rst = await Enchere.findByIdAndUpdate(
+      id,
+      { $set: updatedAnnonce },
+      { new: true }
+    );
+    if (Rst) {
+      await res
+        .status(200)
+        .json({ message: "Enchere updated!", updatedAnnonce });
+    } else {
+      throw new Error("EnchereID undefined !");
     }
-  };
+  } catch (err) {
+    res.status(400).json({ Error: err.message });
+  }
+};
